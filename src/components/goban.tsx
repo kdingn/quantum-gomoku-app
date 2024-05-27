56172a4d3888637ac63d00d459d6c5e63d728bbc
kdingn <koukou3335@gmail.com>
2024-05-27 12:11:25 +0900

    use public env

--- src/components/goban.tsx
+++ src/components/goban.tsx
@@ -1,14 +1,43 @@
-import { getDataFromFirestore } from "../firebase/reader";
-import "../styles/global.css";
-import Gotile from "./gotile";
+"use client";
 
-async function Goban() {
-  const sequence = await getDataFromFirestore("match-00000");
+import Gotile from "@/components/gotile";
+import { db } from "@/firebase/firebase";
+import "@/styles/global.css";
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+function Goban() {
+  const [sequence, setSequence] = useState([]);
+
+  useEffect(() => {
+    const data = collection(db, "match-00000");
+    onSnapshot(data, (querySnapshot) => {
+      setSequence(querySnapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
+  // const sequence = [
+  //   { timestamp: "", i: 4, j: 2, prob: 90 },
+  //   { timestamp: "", i: 3, j: 3, prob: 10 },
+  //   { timestamp: "", i: 2, j: 2, prob: 70 },
+  //   { timestamp: "", i: 3, j: 4, prob: 30 },
+  //   { timestamp: "", i: 1, j: 1, prob: 90 },
+  //   { timestamp: "", i: 1, j: 2, prob: 10 },
+  //   { timestamp: "", i: 3, j: 1, prob: 70 },
+  //   { timestamp: "", i: 13, j: 1, prob: 30 },
+  //   { timestamp: "", i: 1, j: 13, prob: 90 },
+  //   { timestamp: "", i: 2, j: 13, prob: 10 },
+  //   { timestamp: "", i: 13, j: 2, prob: 70 },
+  //   { timestamp: "", i: 13, j: 13, prob: 30 },
+  // ];
+
   const positionProbMap = {};
   sequence.forEach((item) => {
     const key = `${item.i}-${item.j}`;
     positionProbMap[key] = item.prob;
   });
+
+  // console.log(positionProbMap);
 
   const gobanSize = 13;
   const gobanDocument = [];
--- src/firebase/reader.ts
+++
@@ -1,9 +0,0 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
-
-export async function getDataFromFirestore(collectionName) {
-  const colRef = collection(db, collectionName);
-  const snapshot = await getDocs(colRef);
-  const data = snapshot.docs.map((doc) => doc.data());
-  return data;
-}
