c4dfa339851f41ddfd0d90eb99a8b51abaf94a87
kdingn <koukou3335@gmail.com>
2024-05-27 09:48:01 +0900

    remove comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-// import Goban from "@/components/goban";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Link href="/match">match</Link>
-      {/* <div className="goban-container">
-        <Goban />
-      </div> */}
     </div>
   );
 }
