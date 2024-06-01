27b691a500932dc28aa8a7f4d5836d47250f8c73
kdingn <koukou3335@gmail.com>
2024-06-01 17:10:15 +0900

    show username

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <div>
-      <div>{username}</div>
+      <div>login : {username}</div>
       <Link href="/match?id=00000">match</Link>
     </div>
   );
