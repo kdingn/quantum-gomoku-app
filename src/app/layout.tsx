cc705171b9f384db0aa554e3cf9de9439b430c7a
kdingn <koukou3335@gmail.com>
2024-06-01 18:01:24 +0900

    loading page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,57 +3,58 @@
 import Navbar from "@/components/navbar";
 import "@/styles/global.css";
 import Head from "next/head";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
-  // const router = useRouter();
+  const router = useRouter();
   const pathname = usePathname();
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   if (sessionStorage.length === 0) {
-  //     router.push("/signin");
-  //   } else if (sessionStorage.length !== 0 && pathname === "/signin") {
-  //     router.push("/");
-  //   }
-  //   setLoading(false);
-  // }, [pathname]);
+  useEffect(() => {
+    if (sessionStorage.length === 0) {
+      router.push("/sign");
+    } else if (sessionStorage.length !== 0 && pathname === "/sign") {
+      router.push("/");
+    }
+    setLoading(false);
+  }, [pathname]);
 
-  // if (loading) {
-  //   return (
-  //     <html lang="en">
-  //       <body>
-  //         <div className="rootlayout">
-  //           <div>Loading...</div>
-  //         </div>
-  //       </body>
-  //     </html>
-  //   );
-  // }
-
-  return (
-    <html lang="en">
-      <body>
-        <Head>
-          <title>Quantum Gomoku</title>
-          <meta
-            name="description"
-            content="Quantum gomoku is game gomoku with each stone probability of changing their color."
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-        </Head>
-        <Navbar />
-        <div className="container">
+  if (loading) {
+    return (
+      <html lang="en">
+        <body>
           <div className="rootlayout">
-            <div>
-              <main>{children}</main>
+            <div>Loading...</div>
+          </div>
+        </body>
+      </html>
+    );
+  } else {
+    return (
+      <html lang="en">
+        <body>
+          <Head>
+            <title>Quantum Gomoku</title>
+            <meta
+              name="description"
+              content="Quantum gomoku is game gomoku with each stone probability of changing their color."
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+          </Head>
+          <Navbar />
+          <div className="container">
+            <div className="rootlayout">
+              <div>
+                <main>{children}</main>
+              </div>
             </div>
           </div>
-        </div>
-      </body>
-    </html>
-  );
+        </body>
+      </html>
+    );
+  }
 }
