7b05988177f26c48054ffc927e108448f2c126f6
kdingn <koukou3335@gmail.com>
2024-06-01 21:53:42 +0900

    sticky contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,30 @@
 export default function Home() {
   const username = sessionStorage.getItem("username");
 
+  const matchesDocument = [];
+  for (let i = 1; i < 10; i++) {
+    matchesDocument.push(<div className="home-content-matchinfo">aaa</div>);
+  }
+
   return (
     <div>
-      <div>login : {username}</div>
-      <div className="home-content">
-        <span className="home-content-description">*now in preparing...</span>
-        <div className="home-content-center">
-          <span>Play Game with AI</span>
+      <div className="fixed-contents">
+        <div>login : {username}</div>
+        <div className="home-content">
+          <span className="home-content-description">*now in preparing...</span>
+          <div className="home-content-center">
+            <span>Play Game with AI</span>
+          </div>
+        </div>
+        <div className="home-content">
+          <div className="home-content-center">
+            <span>Create Match</span>
+          </div>
         </div>
       </div>
-      <div className="home-content">
-        <div className="home-content-center">
-          <span>Create Match</span>
-        </div>
+      <div>
+        <span className="home-content-matches-title">Matches</span>
+        {matchesDocument}
       </div>
     </div>
   );
