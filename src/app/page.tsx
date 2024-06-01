a32f59672ddddba7335d21549291f4a04dc39711
kdingn <koukou3335@gmail.com>
2024-06-01 20:34:00 +0900

    add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import Link from "next/link";
-
 export default function Home() {
   const username = sessionStorage.getItem("username");
 
   return (
     <div>
       <div>login : {username}</div>
-      <Link href="/match?id=00000">match</Link>
+      <div className="home-content">
+        <span className="home-content-description">*now in preparing...</span>
+        <div className="home-content-center">
+          <span>Play Game with AI</span>
+        </div>
+      </div>
+      <div className="home-content">
+        <div className="home-content-center">
+          <span>Create Match</span>
+        </div>
+      </div>
     </div>
   );
 }
